e7d1958d64a9b61a9102f36e1f69d9f40ef0a469
taimul <taimul.haque@gmail.com>
2023-02-12 21:25:20 +0600

    some tweak

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express= require ('express');
 const cors =require('cors');
-const multer =require('multer');
-const fs = require('file-system');
 const MongoClient = require('mongodb').MongoClient
-// const ObjectId = require('mongodb').ObjectId;
-// const mongoose = require('mongoose')
+
 const app = express();
 const {ServerApiVersion } = require('mongodb');
 
@@ -23,75 +20,12 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://GitFair:MFMi2bXccrNW0uLg@cluster0.wzbzlyu.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wzbzlyu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
 
-//mongoose
-// mongoose.connect('mongodb://localhost:5000/IMGDB2')
-// let myschema = mongoose.Schema({
-//     Picture : String
-// })
-// let mymodel = mongoose.model('table', myschema)
-
-//Storage Setting
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "./public/images/");
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.fieldname + "-" + Date.now());
-//     },
-//   });
-  
-//   const upload = multer({ storage: storage });
- 
- //SINGALE IMAGE UPLODING
-//  const mediaCollection = client.db('Gitfair').collection('images');
-
-// app.post("/upload", upload.single("file"), (req, res, next) => {
-//   const file = req.file;
-//   var img = fs.readFileSync(req.file.path);
-//   var encode_image = img.toString('base64');
-
-//   var finalImg = {
-//     contentType: req.file.mimetype,
-//     image: Buffer.from(encode_image, 'base64')
-//   };  
-
-//       db.collection('images').insertOne(finalImg, (err, result) => {
-//         console.log(result)
-//         if (err) return console.log(err)
-//         console.log('saved to database')
-//         // res.redirect('/')
-//     })
-
-//   if (!file) {
-//     const error = new Error("Please upload a file");
-//     error.httpStatusCode = 400;
-//     return next(error);
-//   }
-//   res.send(file);
-// });
-
-//  app.post('/singlepost', upload.single('myImage'), (req, res) => {
-//     var img = fs.readFileSync(req.file.path);
-//     var encode_image = img.toString('base64');
-//     // Define a JSONobject for the image attributes for saving to database
-
-//     var finalImg = {
-//         contentType: req.file.mimetype,
-//         image: Buffer.from(encode_image, 'base64')
-//     };
-//     db.collection('images').insertOne(finalImg, (err, result) => {
-//         console.log(result)
-//         if (err) return console.log(err)
-//         console.log('saved to database')
-        
-//     })
-// })
 
 async function run(){
     try{
@@ -111,44 +45,7 @@ async function run(){
         res.send(result);
     })
 
-        // app.post("/", upload.single("file"), async(req, res, next) =>{
-        //   const file = req.file;
-        //   console.log(file);
-        //   // const media = req.body;
-        //   const doc = {
-        //     title: "Record of a Shriveled Datum",
-        //     image: req.file.originalname,
-        //   }
-        //   const result = await mediaCollection.insertOne(doc);
-        //   res.send(result);
-          // if (!file) {
-          //       const error = new Error("Please upload a file");
-          //       error.httpStatusCode = 400;
-          //       return next(error);
-          //     }
-
-          //     res.send(file);
-            
-
-       
-      //   app.post('/', async (req, res) => {
-      //     const media = req.body;
-      //     const result = await mediaCollection.insertOne(media);
-      //     res.send(result);
-      // })
-        // app.post('/singlepost',upload.single('single_input'), async (req, res) => {
-        //     const images = req.body;
-        //     const result = await mediaCollection.insertOne(images);
-        //     res.send(result);
-        // })
-
-        //  //get all media
-        //  app.get('/all-media', async (req, res) => {
-        //     const query = {};
-        //     const cursor = mediaCollection.find(query);
-        //     const result = await cursor.toArray();
-        //     res.send(result);
-        // })
+        
     }
     finally{
 
